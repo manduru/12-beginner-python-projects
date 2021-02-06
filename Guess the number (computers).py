af0b926112bdc065de35486050851141e9d3556e
Manduru <mitchelleanduru@gmail.com>
2021-02-06 11:51:11 +0300

    Rock, paper, scissors

--- Guess the number (computers).py
+++ Guess the number (computers).py
@@ -3,11 +3,28 @@ import random
 def guess(x):
     random_number = random, randint(1, x)
     guess = 0
-    while guess != random_number:
+    while guess != random_number:    
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
             print('Sorry, guess again. Too low. ')
         elif guess > random_number:
             print('Sorry, guess again. Too high. ')
 
-    Print('Yay, congrats. You have guessed the number{random_number}correctly')
+    print('Yay!, congrats. You have guessed the number{random_number}correctly')
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c' and low != high:
+        if low != high:
+            guess = random.randint(low, high)
+        else:    
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1 
+    print('Yay! The computer guessed your number, {guess}, correctly!')
+
+computer_guess(1000)                   
